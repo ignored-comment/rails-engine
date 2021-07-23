cf175b8a822a5e0d0692b43ce158a0c21763d031
ignored-comment <info.konham@gmail.com>
2021-07-23 00:51:23 -0400

    begin validations for tables; tests failing

---
+++ spec/models/customer_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Customer do
+    describe 'relationships' do
+        it { should have_many(:invoices) }
+        it { should have_many(:merchants).through(:invoices) }
+        it { should have_many(:transactions).through(:invoices) }
+        
+    end
+end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -4,4 +4,10 @@ RSpec.describe Merchant do
     describe 'relationships' do
         it { should have_many(:items) }
     end
+
+    describe 'instance methods' do
+        it 'can calculate revenue of a single merchant' do
+            
+        end
+    end
 end
